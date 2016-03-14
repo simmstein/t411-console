c194aa1a8fd8903eaeb49e5caee30857c8979fb7
Jeoffrey Bauvin <jeoffrey.bauvin.ext@boursorama.fr>
2016-03-14 15:29:15 +0100

    Change api.t411.in to api.t411.ch

--- src/Api/Client.php
+++ src/Api/Client.php
@@ -13,7 +13,7 @@ class Client
 
     public function __construct()
     {
-        $this->client = new GuzzleClient(['base_url' => 'https://api.t411.in']);
+        $this->client = new GuzzleClient(['base_url' => 'https://api.t411.ch']);
     }
 
     public function getAuthorization($username, $password)
