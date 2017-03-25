ecb6b89518094e2ad3249fb37f3565ec4be3b6e3
Simon Vieille <simon@deblan.fr>
2017-03-25 22:14:50 +0100

    Change api.t411.li to api.t411.ai

--- src/Api/Client.php
+++ src/Api/Client.php
@@ -13,7 +13,7 @@ class Client
 
     public function __construct()
     {
-        $this->client = new GuzzleClient(['base_url' => 'https://api.t411.li']);
+        $this->client = new GuzzleClient(['base_url' => 'https://api.t411.ai']);
     }
 
     public function getAuthorization($username, $password)
